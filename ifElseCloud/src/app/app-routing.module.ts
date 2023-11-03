a5813998d55ebda905d1160f3af6f7b16884ad79
Mahadev <khopkarmahadev8468@gmail.com>
2023-11-03 22:32:35 +0530

    invoke

--- ifElseCloud/src/app/app-routing.module.ts
+++ ifElseCloud/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TopDataCardDashboardComponent } from './top-data-card-dashboard/top-data-card-dashboard.component';
 
 const routes: Routes = [
   {path :'',component:HomeComponent},
+  {path :'topDataCardDashboard',component:TopDataCardDashboardComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
